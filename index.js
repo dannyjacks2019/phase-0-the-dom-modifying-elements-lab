fb148f2cbc854a6f2613caaaa09e6163698d0617
Dan Jackson <dannyjacks2019@gmail.com>
2023-04-12 22:00:38 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector('#main')
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = "Dan is the champion"
+Document.body.append(newHeader)
